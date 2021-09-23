38dca554211312c213afd343309b3aca6c13376b
Victor S <victor.skarerverket@gmail.com>
2021-09-23 12:09:39 +0200

    Added character and room calsses and some user input code in main

--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,10 @@
 public class Character {
+    String name;
+    int hp;
+
+    public Character (String name){
+        name = name;
+        int age;
+        hp = 25;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please type in your name: ");
+        String userInput = scanner.nextLine();
+
+
+
+        Character player = new Character(userInput);
+        System.out.println("Your name is " + userInput + ". Is that right?");
+
+        if (scanner.nextLine().toLowerCase() == "yes" || scanner.nextLine().toLowerCase() == "y"){
+             // create character with userInputs name as name
+        }
 
     }
 }
