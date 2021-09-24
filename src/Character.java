f1f94396cb819384801dcba62136ebc5af80e8e5
Victor S <victor.skarerverket@gmail.com>
2021-09-24 12:49:15 +0200

    Finished D&D Style character stat generator

--- src/Character.java
+++ src/Character.java
@@ -1,7 +1,6 @@
 public class Character {
     private String name;
     private int hp = 100;
-    private int[] stats;
     private int strength;
     private int agility;
     // Strength + HP + Strength Weapon Dmg
@@ -19,34 +18,33 @@ public class Character {
 
     public Character(String charName){
         name = charName;
-        strength = 8;
-        agility = 8;
-        characterStatGenerator();
+        strength = characterStatGenerator();
+        agility = characterStatGenerator();
     }
 
-    public void characterStatGenerator(){
-        int[] sumOfRolls = new int[4];
+    // rolls 4 Six Sided die, ignores the lowest roll, and adds them together.
+    public int characterStatGenerator(){
 
+        int[] sumOfRolls = new int[4];
+        int lowestRoll = 6;
         int minRoll = 1;
         int maxRoll = 6;
         int range = maxRoll - minRoll +1;
+
         for (int i = 0; i < 4; i++){
             int diceResult = (int)(Math.random() * range ) + minRoll;
             sumOfRolls[i] = diceResult;
-        }
-        // now remove smallest val in array
-
-        for (int i = 0; i < sumOfRolls.length; i++){
-
+            // Get the lowest dice roll to subtract later.
+            if (lowestRoll > diceResult)
+                lowestRoll = diceResult;
         }
 
-
-
-        //roll 4d6, ignore the lowest roll, and add them together.
-        // For example, if you roll a 4 5 2 6, you ignore the 2, and you’ve got 15.
-        //You do that 6 times, pool them all together,
-        // and distribute them however you’d like
-        // (this is to avoid a Wizard having an awkwardly high Strength because you rolled well the first time).
-
+        // We subtract the lowest dice roll.
+        int sum = -lowestRoll;
+        for (int dice : sumOfRolls){
+            System.out.print(dice);
+            sum = sum + dice;
+        }
+        return sum;
     }
 }
