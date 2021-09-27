f491fab6a8e4a6b30a36fba3ad3afd7b3e6840b7
Victor S <victor.skarerverket@gmail.com>
2021-09-27 17:37:59 +0200

    Started work on menu and made a roomList for all the rooms to go in.

--- src/GameStart.java
+++ src/GameStart.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class GameStart {
 
+    ArrayList<Room> roomList = new ArrayList<Room>();
+
     public void initialization(){
+
         Character player = new Character("TestChar");
         System.out.println("Your Strength is: " + player.getStrength());
         System.out.println("Your Agility is: " + player.getAgility());
@@ -17,6 +22,8 @@ public class GameStart {
         Character enemy2 = new Character("Will-O'-Wisp", 5, 28);
 
         Room room1 = new Room("Entrance");
+        roomList.add(room1);
+
         room1.charArrayList.add(enemy1);
         room1.charArrayList.add(enemy2);
 
@@ -34,34 +41,28 @@ public class GameStart {
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine().toLowerCase(Locale.ROOT);
-        String[] inputArray = input.split(" ");
+        String[] commandParts = input.split(" ");
 
-        for (int i = 0; i < inputArray.length; i++){
-            if (inputArray[i].contains("go")){
-                switch (inputArray[++i]) {
+        for (int i = 0; i < commandParts.length; i++){
+            if (commandParts[i].contains("go") && commandParts.length > i+1 ){
+                switch (commandParts[i++]) {
                     case "north":
-
+                        // check if there's a door there
+                        System.out.println("second word should be north");
                     case "east":
-
+                        System.out.println("second word should be east");
                     case "south":
-                        
+                        System.out.println("second word should be south");
                     case "west":
-
+                        System.out.println("second word should be west");
                     default:
+                        System.out.println("there was no landmark direction after 'go'");
                 }
             }
+            if (commandParts[i].contains("exit") || commandParts[i].contains("quit")){
+                exitGame();
+            }
         }
-
-
-        switch (input) {
-            case "help":
-                // List commands
-                ;
-
-            case "go":
-        }
-
-        exitGame();
     }
 
     public void exitGame(){
