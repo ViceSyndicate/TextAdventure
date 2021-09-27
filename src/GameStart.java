df94eb910e55c45386f7e83b1b3af2b98f149fc5
Victor S <victor.skarerverket@gmail.com>
2021-09-27 16:03:23 +0200

    Started working on user input and menu

--- src/GameStart.java
+++ src/GameStart.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class GameStart {
 
     public void initialization(){
@@ -7,8 +10,9 @@ public class GameStart {
 
         Weapon dagger = new Weapon("Dagger", 1,4);
         dagger.description = "Short Pointy Sword.";
+        Weapon mace = new Weapon("Mace",1, 6);
+        mace.description = "Bonk em with this";
 
-        
         Character enemy1 = new Character("Mammoth", 24, 9);
         Character enemy2 = new Character("Will-O'-Wisp", 5, 28);
 
@@ -19,11 +23,45 @@ public class GameStart {
         room1.charList.add(enemy1);
         room1.charList.add(enemy2);
 
-        System.out.println(room1.name);
+        gameLoop();
     }
 
     public void gameLoop(){
 
+        System.out.println("You wake up in a dimly lit cave");
+        System.out.println("You can interact with this game using text commands.");
+        System.out.println("Write 'help' for some commands");
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toLowerCase(Locale.ROOT);
+        String[] inputArray = input.split(" ");
+
+        for (int i = 0; i < inputArray.length; i++){
+            if (inputArray[i].contains("go")){
+                switch (inputArray[++i]) {
+                    case "north":
+
+                    case "east":
+
+                    case "south":
+                        
+                    case "west":
+
+                    default:
+                }
+            }
+        }
+
+
+        switch (input) {
+            case "help":
+                // List commands
+                ;
+
+            case "go":
+        }
+
+        exitGame();
     }
 
     public void exitGame(){
